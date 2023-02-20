919045aed9e0c891b2621013fedb55af4e1684fa
onionmccabbage@live.com <onionmccabbage@live.com>
2023-02-20 16:07:42 +0000

    near the end of day 1

---
+++ 01_typings/04_mappings.ts
@@ -0,0 +1,40 @@
+namespace Options_Verbose { // modern ES lets us use the keyword 'namespace'
+    interface Options {
+        material: string
+        backlight:boolean
+    }
+    // read-only version
+    interface ManualReadOnlyOptions {
+        readonly material:string
+        readonly backlight:boolean
+    }
+    // optional version
+    interface ManualOptionalOptions {
+        material?:string
+        backlight?:boolean
+    }
+    // nullable version
+    interface ManualNullableOptions {
+        material:string | null
+        backlight:boolean | null
+    }
+}
+
+namespace Options_Terse {
+    interface Options { // our 'base' type
+        material: string
+        backlight:boolean
+    }
+    // we can use mapping to make new types from our 'base' type
+    type ReadOnlyOptions<T> = { readonly [k in keyof T]: T[k] }
+    type OptionalOptions<T> = { [k in keyof T]? : T[k] }
+    type NullableOptions<T> = { [k in keyof T] : T[k] | null }
+
+    // then...
+    type ReadOnly = ReadOnlyOptions<Options> // we explicitly set 'T' to Options here
+    type Optional = OptionalOptions<Options>
+    type Nullable = NullableOptions<Options>
+
+
+    
+}
