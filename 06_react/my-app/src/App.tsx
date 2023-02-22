651682f5cd3d304dba0a2b07470e6e4dd43f440a
onionmccabbage@live.com <onionmccabbage@live.com>
2023-02-22 15:14:22 +0000

    after tea day 3

--- 06_react/my-app/src/App.tsx
+++ 06_react/my-app/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// import {Photo, fetchUsers} from './tryit'
+
+
 const App = () => {
   return (
     <div className="App">
@@ -23,4 +26,28 @@ const App = () => {
   );
 }
 
+
 export default App;
+
+export interface Photo {
+  albumId      : number
+  id           : number
+  title        : string
+  url          : string
+  thumbnailUrl : string
+}
+
+const fetchUsers = async <T,>(): Promise< Array<T> | string | undefined >=>{ 
+  // const fetchUsers = async (): Promise< Array<Photo> | string | undefined >=>{ 
+    const API = `https://jsonplaceholder.typicode.com/users`
+    try {
+        const response = await fetch(API)
+        return response.json() 
+    }
+    catch (err) {
+        if (err) {
+            return err as string //err.message
+        }
+    }
+    finally {}
+}
