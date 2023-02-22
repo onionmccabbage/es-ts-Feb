cccb624cb4ba8459c08c0d9d79e56b5afac5f1c0
onionmccabbage@live.com <onionmccabbage@live.com>
2023-02-22 11:57:53 +0000

    working observable

--- 07_observables/src/app.ts
+++ 07_observables/src/app.ts
@@ -1,4 +1,5 @@
-
+import { fromEvent, debounceTime, map, tap } from 'rxjs' // TSC will use node to resolve where this is - its a node module
+// import { debounceTime, map, tap } from 'rxjs/operators'
 
 // here are some potential search strings for our user
 const testData:string[] = [
@@ -12,3 +13,18 @@ const testData:string[] = [
 // const searchBox = document.querySelector<HTMLInputElement>('#search')
 // const searchBox:HTMLInputElement = document.querySelector('#search')
 const searchBox = document.querySelector('#search') as HTMLInputElement // cast for TS accuracy
+
+// we can make an observable called keyStream$ - the $ is conventional
+const keyStream$ = fromEvent(searchBox, "keyup")
+// now we use the observable
+keyStream$.pipe(
+    map( (event)=>{
+        const input = event.target as HTMLInputElement // the key-event will have come from a target element
+        return input.value //this will be the ENTIRE contents of the search field
+    } ),
+    tap( (item)=>{console.log(`we recieved ${item}`)} )
+).subscribe( ()=>{} )
+
+
+
+
